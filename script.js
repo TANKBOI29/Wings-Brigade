5e83dcdd6bc298972634b5599a40601ae74cbf29
TANKBOI29 <154932586+TANKBOI29@users.noreply.github.com>
2024-11-04 19:12:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elements = document.querySelectorAll('.slide-in, .fade-in');
 
-    const scrollHandler = () => {
-        elements.forEach(element => {
-            const position = element.getBoundingClientRect().top;
-            if (position < window.innerHeight * 0.9) {
-                element.classList.add('visible');
-            }
-        });
-    };
-
-    window.addEventListener('scroll', scrollHandler);
-    scrollHandler(); 
-});
